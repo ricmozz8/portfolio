d7b75f70212f5137d2a7eca92c7a74d2a6e59430
ricmozz8 <cupcakethief2311@gmail.com>
2025-03-13 20:55:55 -0400

    Remove view transition logic for internal links in viewTransition.js

--- js/viewTransition.js
+++ js/viewTransition.js
@@ -5,20 +5,7 @@
  * prevents the default behavior, fades out the current page, and then navigates to the new page after a short delay.
  *
  */
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('a[href]').forEach(a => {
-    a.addEventListener('click', e => {
-      if (a.getAttribute('href') !== '#' && a.getAttribute('href') !== window.location.href && !a.getAttribute('href').startsWith('http')) {
-        e.preventDefault();
-        document.body.style.transition = 'opacity 0.5s ease';
-        document.body.style.opacity = 0;
-        setTimeout(() => {
-          window.location.href = a.href;
-        }, 500);
-      }
-    });
-  });
-});
+
 
 
 function scrollToSection(id) {
